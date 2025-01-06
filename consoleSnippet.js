754e3630001605e2ee4bfe76e0ba854838ff4a65
Revanray1 <90889608+Revanray1@users.noreply.github.com>
2025-01-06 16:35:34 +0530

    Create consoleSnippet.js

---
+++ consoleSnippet.js
@@ -0,0 +1,15 @@
+1. 
+const {length} = "Sri";
+
+console.log(length); // 3 Access string Object length property
+
+2. 
+if(1<4<1){
+console.log("Print") // false and won't prints console
+}
+
+if(1<4<3){  
+console.log("Print")    // true and prints console
+}
+
+js reads conditopn from left to right , 1<4 = true, here true value = 1 --> 1<3 true it will become (1<3)
