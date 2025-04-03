f0fe9913a78a245d7abbe697ebe8941a8f4bbd3f
Revanray1 <90889608+Revanray1@users.noreply.github.com>
2025-04-03 14:54:08 +0530

    Update reverseEachCharacterInSentencebutNotSpace.js

--- reverseEachCharacterInSentencebutNotSpace.js
+++ reverseEachCharacterInSentencebutNotSpace.js
@@ -23,6 +23,40 @@ function reverseSentence(data) {
 reverseSentence(sentence); 
 
 
+
+
+
+// ANOTHER METHOD 
+
+const a = "here iss the new challenge is to"
+
+const b = a.split(" ");
+console.log(b.length)
+
+let c = "", index=a.length-1;
+
+for(let i=0;i<b.length;i++){
+
+    for(let j=0;j<b[i].length;j++){
+
+        if(a[index]===" "){
+
+            index-=1;
+
+        }
+
+        c+= a[index];
+
+        index-=1;
+
+    }
+console.log(c)
+    c+=" "
+
+}
+
+console.log(c)
+
 OUTPUT :
 Input  : here is the new Challenge is
 Output : sieg ne lla hCw enehtsier eh
