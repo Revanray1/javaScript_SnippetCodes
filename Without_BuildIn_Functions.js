30f73034a153773f50b5f939cb4f88a9eaac3293
Revanray1 <90889608+Revanray1@users.noreply.github.com>
2025-05-02 11:08:35 +0530

    Create Without_BuildIn_Functions.js

---
+++ Without_BuildIn_Functions.js
@@ -0,0 +1,24 @@
+without Inbuiit Functions
+
+// MAP and FILTER
+
+const data = [{name:"Ayush", age:22, course:"BSC"},{name:"Chal", age:24, course:"Maths"}
+,{name:"deloti", age:21, course:"BAC"},{name:"agarkhan", age:27, course:"B.com"}]
+
+function mapData(data, callBack){
+  let temp_array =[];
+  
+  for(i=0;i<data.length;i++){
+    if(callBack(data[i],i,data)){
+    temp_array.push(data[i])
+    }
+  }
+  return temp_array
+  
+}
+
+const callBack = (d)=>( d.course !== "BSC")
+
+const result = mapData(data,callBack)
+
+console.log(result)
